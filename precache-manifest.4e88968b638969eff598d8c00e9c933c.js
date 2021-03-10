722d48019987ca5f9558c9b6680b1aff3ec8999a
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-10 11:14:29 +0200

    Updates

---
+++ precache-manifest.4e88968b638969eff598d8c00e9c933c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718e3ac7ebb695410f6f6167065480f4",
+    "url": "/Lirten1/index.html"
+  },
+  {
+    "revision": "eaee5399d001312d53fb",
+    "url": "/Lirten1/static/css/main.1bb9ae04.chunk.css"
+  },
+  {
+    "revision": "0ea688a25303f7ba39a8",
+    "url": "/Lirten1/static/js/2.95d61421.chunk.js"
+  },
+  {
+    "revision": "eaee5399d001312d53fb",
+    "url": "/Lirten1/static/js/main.c91ad1ca.chunk.js"
+  },
+  {
+    "revision": "20604d2c2fce9f2a5cea",
+    "url": "/Lirten1/static/js/runtime-main.577c2b40.js"
+  },
+  {
+    "revision": "9efe260fd0461c3318d6ff5cc5b1ebd4",
+    "url": "/Lirten1/static/media/01.9efe260f.png"
+  },
+  {
+    "revision": "adbd1a3bcf16d2dd474ce649d75a7b27",
+    "url": "/Lirten1/static/media/Rectangle 46.adbd1a3b.png"
+  },
+  {
+    "revision": "c800dfb93caff504f69533059f174476",
+    "url": "/Lirten1/static/media/Rectangle 50.c800dfb9.png"
+  },
+  {
+    "revision": "345add9910d0bde1e3dd93e5b9ffd3bd",
+    "url": "/Lirten1/static/media/Rectangle 59.345add99.png"
+  },
+  {
+    "revision": "30b477e10ace8ef69031990297e68426",
+    "url": "/Lirten1/static/media/Rectangle57.30b477e1.png"
+  },
+  {
+    "revision": "0342d93cbf32a4f230310b958a284319",
+    "url": "/Lirten1/static/media/SOLUTIONS.0342d93c.png"
+  },
+  {
+    "revision": "3bd45dae7d75624598fd8584b3f36908",
+    "url": "/Lirten1/static/media/Services.3bd45dae.png"
+  },
+  {
+    "revision": "02c674a3ac02a80acdebb94a420c0dc3",
+    "url": "/Lirten1/static/media/aboutBg3.02c674a3.png"
+  },
+  {
+    "revision": "d4806b36fb21af0bd3b11b361a64065e",
+    "url": "/Lirten1/static/media/aboutBg4.d4806b36.png"
+  },
+  {
+    "revision": "40adcdaac8ff004291a5673372124ffd",
+    "url": "/Lirten1/static/media/blur-home.40adcdaa.png"
+  },
+  {
+    "revision": "31b39e45775b73f431e087f776c17b67",
+    "url": "/Lirten1/static/media/brandimg.31b39e45.png"
+  },
+  {
+    "revision": "9eaec7b51968a463beb2171db47a1d22",
+    "url": "/Lirten1/static/media/code-header.9eaec7b5.png"
+  },
+  {
+    "revision": "63dc358ce306a678caea3b53ede348d7",
+    "url": "/Lirten1/static/media/contact-header.63dc358c.png"
+  },
+  {
+    "revision": "b0ba9ea5a540e6b8e01550d1ada109f5",
+    "url": "/Lirten1/static/media/design-circle.b0ba9ea5.png"
+  },
+  {
+    "revision": "00650807eee11edba42f862609c79f0b",
+    "url": "/Lirten1/static/media/design-header.00650807.png"
+  },
+  {
+    "revision": "605e9701fe78a69aecd309da5116bd27",
+    "url": "/Lirten1/static/media/designBg5.605e9701.png"
+  },
+  {
+    "revision": "247bb1fc378c8ec3d9546e9b0e636a1e",
+    "url": "/Lirten1/static/media/home-header.247bb1fc.png"
+  },
+  {
+    "revision": "db3ea65d827050c990d55e96a8aa68b9",
+    "url": "/Lirten1/static/media/join-header.db3ea65d.png"
+  },
+  {
+    "revision": "b3ba3642422eb83d470a8ce484dd5188",
+    "url": "/Lirten1/static/media/lines.b3ba3642.png"
+  },
+  {
+    "revision": "aecee295de8449ea2497aca34611ccd5",
+    "url": "/Lirten1/static/media/manage-header.aecee295.png"
+  },
+  {
+    "revision": "1efc25039fcde40b68c0e571cfd48f44",
+    "url": "/Lirten1/static/media/manageImg1.1efc2503.png"
+  },
+  {
+    "revision": "6077cf8660dd33c6cc1d045915503b29",
+    "url": "/Lirten1/static/media/manageImg2.6077cf86.png"
+  },
+  {
+    "revision": "5eb97ef3c81b57c83eeec07513927e98",
+    "url": "/Lirten1/static/media/manageImg3.5eb97ef3.png"
+  },
+  {
+    "revision": "24e44f7dfc59f9878c94a09e97bb7a6c",
+    "url": "/Lirten1/static/media/offering.24e44f7d.png"
+  },
+  {
+    "revision": "58dfbc62db88c4d4c73afa0e09b73038",
+    "url": "/Lirten1/static/media/planning.58dfbc62.png"
+  },
+  {
+    "revision": "4e2ef30e365ff9cb8c1ae31c8dfc4fae",
+    "url": "/Lirten1/static/media/planningBg.4e2ef30e.png"
+  },
+  {
+    "revision": "6078288449d5f365c704d58a5f401a24",
+    "url": "/Lirten1/static/media/process.60782884.png"
+  },
+  {
+    "revision": "7e797006f98f489851b7e782a819acfa",
+    "url": "/Lirten1/static/media/technologies.7e797006.png"
+  },
+  {
+    "revision": "2911fb8b5b77354250e44f8456eb3dab",
+    "url": "/Lirten1/static/media/zero.2911fb8b.png"
+  }
+]);
